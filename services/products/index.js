b1a0df49284fc0a06a62b7525b08eece2c898693
maidersonn <maidersonn@gmail.com>
2021-10-05 19:28:19 +0200

    filename change in products

--- services/products/index.js
+++ services/products/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 module.exports = (db) => {
-  router.get("/keyword", require("./get_by_keyword")(db));
+  router.get("/keyword", require("./get-by-keyword")(db));
 
   return router;
 };
