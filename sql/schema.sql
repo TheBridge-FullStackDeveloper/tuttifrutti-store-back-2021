cf8c0cf02a00d0ad67d2e9b73e50cca0b5e8558e
TommyTraddles <jandresnova@gmail.com>
2021-10-05 22:23:15 +0200

    feat: creating script db:create for dev enviroment

--- sql/schema.sql
+++ sql/schema.sql
@@ -31,16 +31,16 @@ CREATE TABLE IF NOT EXISTS products (
 
 CREATE TABLE IF NOT EXISTS users (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name TEXT NOT NULL,
-  surname TEXT NOT NULL,
+  name TEXT,
+  surname TEXT,
   email TEXT UNIQUE NOT NULL,
   username TEXT UNIQUE NOT NULL,
   hash TEXT NOT NULL,
   access_token TEXT,  
   activation_token TEXT,
   active BOOLEAN NOT NULL DEFAULT false,
-  address TEXT NOT NULL, 
-  postal_code TEXT NOT NULL,
+  address TEXT, 
+  postal_code TEXT,
   profile_pic TEXT,
   created_at TIMESTAMP NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
   updated_at TIMESTAMP NOT NULL DEFAULT (now() AT TIME ZONE 'UTC')
