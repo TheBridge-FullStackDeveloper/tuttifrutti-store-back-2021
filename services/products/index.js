afb2576a46f471de285568e7c40a3f8e2703f437
diegoztrombone <diegoztrombone@gmail.com>
2021-10-06 20:42:44 +0200

    fix: nuevo hot fix, añadiendo enpoit de Juan

--- services/products/index.js
+++ services/products/index.js
@@ -1,16 +1,12 @@
 const router = require("express").Router();
 
 module.exports = (db) => {
-<<<<<<< HEAD
-    router.post('/', require('./get-by-search')(db))
-    router.get("/keyword", require("./get-by-keyword")(db));
-=======
   router.get("/keyword", require("./get-by-keyword")(db));
 
+  router.get("/search", require("./get-by-search")(db));
   router.get("/featured", require("./get-featured")(db));
 
   router.get("/", require("./getProducts")(db));
->>>>>>> 71753ed67b1b34dd17991d77353a082038c290dc
 
     return router;
 };
