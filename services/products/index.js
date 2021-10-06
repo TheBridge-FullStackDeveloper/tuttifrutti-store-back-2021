b600392e382cbddb6e7016b1d664d9a16ce217e5
Juan Anton <jantonperez1980@gmail.com>
2021-10-06 19:30:12 +0200

    modified: services/products/index

--- services/products/index.js
+++ services/products/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 
 module.exports = (db) => {
   router.get("/keyword", require("./get-by-keyword")(db));
-  router.get("/featured", require("./get-featured")(db));
   router.get("/", require("./getProducts")(db));
 
   return router;
