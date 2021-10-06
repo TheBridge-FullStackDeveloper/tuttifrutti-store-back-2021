732b597cc43f510d72ed2bb6e63ac1592e9d634a
maidersonn <maidersonn@gmail.com>
2021-10-06 20:02:20 +0200

    feat: card_number turned into hash

--- services/cards/create-card.js
+++ services/cards/create-card.js
@@ -1,17 +1,20 @@
 const { createCard } = require("../../query/cards");
+const { hash } = require("../../helpers");
 
-const newCard = (db) => async (res, req, next) => {
+const newCard = (db) => async (req, res, next) => {
   const { provider, card_number, expiration_date, owner_name } = req.body;
 
   if (!provider || !card_number || !expiration_date || !owner_name) {
     return next({ error: new Error("Given data failed") });
   }
 
-  const result = await createCard({
+  const cardNumber = await hash.encrypt(card_number);
+
+  const result = await createCard(db, {
     provider,
-    card_number,
-    expiration_date,
-    owner_name,
+    cardNumber,
+    expirationDate: expiration_date,
+    ownerName: owner_name,
   });
 
   if (!result) {
