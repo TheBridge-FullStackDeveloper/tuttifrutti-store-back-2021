3024c9396920fd5e6fcace3920def19eba125cd2
diegoztrombone <diegoztrombone@gmail.com>
2021-10-06 20:45:17 +0200

    fix: añadiendo linea que se había eliminado

--- services/products/index.js
+++ services/products/index.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 
 module.exports = (db) => {
   router.get("/keyword", require("./get-by-keyword")(db));
-
-  router.get("/search", require("./get-by-search")(db));
+  router.post("/", require("./get-by-search")(db));
   router.get("/featured", require("./get-featured")(db));
-
   router.get("/", require("./getProducts")(db));
 
     return router;
