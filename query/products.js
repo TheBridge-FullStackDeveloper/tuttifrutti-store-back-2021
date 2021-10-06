7728feb08a0241f0d1b2ec9019a15280393893cb
Juan Anton <jantonperez1980@gmail.com>
2021-10-06 19:20:26 +0200

    merge failed

--- query/products.js
+++ query/products.js
@@ -25,7 +25,35 @@ const getByKeyword = async (db, { keyword }) => {
   }
 };
 
+const countAll = async (db) => {
+	return await db.query(sql`
+		SELECT *
+		FROM products
+	`);
+};
+
+const getAll = async (db, { page, perPage }) => {
+	const offset = page * perPage - perPage;
+	try {
+		const { rows: query } = await db.query(sql`
+			SELECT *
+			FROM products
+			LIMIT ${perPage}
+			OFFSET ${offset}
+		`);
+		const { rowCount: items } = await countAll(db);
+		return { query, items };
+	} catch (error) {
+		console.info("Error getAll products: ", error.message);
+		return false;
+	}
+};
+
+
 module.exports = { 
+  
     getByKeyword,
+    getAll,
     getFeatured, 
+    
 };
