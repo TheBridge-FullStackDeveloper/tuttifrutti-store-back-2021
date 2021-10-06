65d2e0c4fac68ae75ff4409ae0c4b5d3a35e2987
maidersonn <maidersonn@gmail.com>
2021-10-06 21:28:48 +0200

    chore:query && fix:if condition of req.body incoming data

--- query/auth.js
+++ query/auth.js
@@ -1,9 +1,14 @@
 const { sql } = require("slonik");
 
-const getUserByEmail = async (db, mail, comparationFn) => {
+const getUserByEmailOrUsername = async (
+  db,
+  mail = " ",
+  username = " ",
+  comparationFn
+) => {
   try {
     const result = await db.one(
-      sql`SELECT email, username, hash FROM users WHERE email LIKE ${mail}`
+      sql`SELECT email, username, hash FROM users WHERE email LIKE ${mail} OR username LIKE ${username}`
     );
 
     if (!result) {
@@ -23,27 +28,4 @@ const getUserByEmail = async (db, mail, comparationFn) => {
   }
 };
 
-const getUserByUsername = async (db, name, comparationFn) => {
-  try {
-    const result = await db.one(
-      sql`SELECT email, username, hash FROM users WHERE username LIKE ${name}`
-    );
-
-    if (!result) {
-      throw new Error("invalid credentials");
-    }
-
-    const isValidPassword = await comparationFn(result.hash);
-
-    if (!isValidPassword) {
-      throw new Error("invalid credentials");
-    }
-
-    return result;
-  } catch (error) {
-    console.info("error at getUserByUsername query:", error.message);
-    return false;
-  }
-};
-
-module.exports = { getUserByEmail, getUserByUsername };
+module.exports = { getUserByEmailOrUsername };
--- services/auth/login.js
+++ services/auth/login.js
@@ -1,18 +1,22 @@
 const { hash, serialize } = require("../../helpers");
-const { getUserByEmail, getUserByUsername } = require("../../query/auth");
+const { getUserByEmailOrUsername } = require("../../query/auth");
 
 const login = (db) => async (req, res, next) => {
   const { email, username, password } = req.body;
 
-  if (!email || !username || !password) {
+  if ((!email && !username) || !password) {
     return next({ error: new Error("Given data failed") });
   }
 
-  let user;
-  if (email) {
-    user = await getUserByEmail(db, email, hash.compare(password));
-  } else {
-    user = await getUserByUsername(db, username, hash.compare(password));
+  let user = await getUserByEmailOrUsername(
+    db,
+    email,
+    username,
+    hash.compare(password)
+  );
+
+  if (!user) {
+    return next({ error: new Error("Something went wrong") });
   }
 
   const token = serialize(res, {
@@ -20,13 +24,9 @@ const login = (db) => async (req, res, next) => {
     username: user.username,
   });
 
-  if (!user) {
-    return next({ error: new Error("Something went wrong") });
-  }
-
   res.status(200).json({
     success: true,
-    data: token,
+    data: { access_token: token },
   });
 };
 
