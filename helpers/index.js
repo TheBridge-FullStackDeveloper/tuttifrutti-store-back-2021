122bf17cf047dde814d1ce4b038938f1d6ee61d7
maidersonn <maidersonn@gmail.com>
2021-10-07 21:34:03 +0200

    feat: jwt and deserialize

--- helpers/index.js
+++ helpers/index.js
@@ -1,13 +1,17 @@
 const { encrypt, compare } = require("./hash");
-const { toJWT } = require("./jwt");
+const { toJWT, fromJWT } = require("./jwt");
 
 const serialize = (res, { email, username }) => {
   const accessToken = toJWT(email, username);
   return accessToken;
 };
 
+const deserialize = (token) => {
+  return fromJWT(token);
+};
+
 module.exports = {
   serialize,
-
+  deserialize,
   hash: { encrypt, compare },
 };
