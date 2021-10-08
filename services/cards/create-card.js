5c7135c22d315923eb320b0bdc993befb84a7e48
maidersonn <maidersonn@gmail.com>
2021-10-08 16:12:53 +0200

    change: validation middleware

--- services/cards/create-card.js
+++ services/cards/create-card.js
@@ -5,10 +5,6 @@ const newCard = (db) => async (req, res, next) => {
   const { provider, card_number, expiration_date, owner_name } = req.body;
   const { email, username } = res.locals.user;
 
-  if (!provider || !card_number || !expiration_date || !owner_name) {
-    return next({ error: new Error("Given data failed") });
-  }
-
   const cardNumber = await hash.encrypt(card_number);
 
   const result = await createCard(db, {
--- services/cards/index.js
+++ services/cards/index.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const { authorization } = require("../../middlewares/authorizer");
+const createCardValidation = require("../../middlewares/createCardValidator");
 
 module.exports = (db) => {
-  router.post("/new", authorization, require("./create-card")(db));
+  router.post(
+    "/new",
+    authorization,
+    createCardValidation,
+    require("./create-card")(db)
+  );
 
   return router;
 };
