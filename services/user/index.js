700a20e5821bf5185eae62a079ba3973a47c6e81
Juan Anton <jantonperez1980@gmail.com>
2021-10-13 20:54:40 +0200

    feat: update.js from user

--- services/user/index.js
+++ services/user/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 module.exports = db => {
     const update = require("./update")
 
-    router.put('/', update(db))
+    router.put('/update', update(db))
 
 
     return router
