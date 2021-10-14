62d95f32195dd2b13e5db1fd36d03617734f8ce4
maidersonn <maidersonn@gmail.com>
2021-10-14 20:12:41 +0200

    feat:sent new token to user email

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,10 +1,9 @@
 // const { sendActivationMail } = require('./templates/activation')
 // const { sendConfirmationMail } = require('./templates/confirmation')
-
-
-
+// const { sendPasswordUpdate } = require('./templates/passwordUpdate')
 
 // module.exports = {
 //   sendActivationMail,
 //   sendConfirmationMail,
+//   sendPasswordUpdate,
 // }
---
+++ services/auth/password_forgotten.js
@@ -0,0 +1,34 @@
+const { hash } = require('../../helpers');
+const { sendMail } = require('../../config/mailer');
+const { getUserData } = require('../../query/user');
+
+const passwordForgotten = (db) => async (req, res, next) => {
+  const { email, username } = req.body;
+
+  if (!email && !username) {
+    return next({ error: new Error('Given data failed.') });
+  }
+
+  const user = await getUserData(db, { email, username });
+
+  if (!user) {
+    return next({ error: new Error('User does not exist.') });
+  }
+
+  const token = hash.createToken();
+
+  const newToken = await updateToken(db, token, { email, username });
+
+  if (!newToken) {
+    return next({ error: new Error('Something went wrong.') });
+  }
+
+  await sendMail.passwordForgotten({ email: user.email, token });
+
+  res.status(200).json({
+    success: true,
+    info: 'Email has been sent.',
+  });
+};
+
+module.exports = passwordForgotten;
