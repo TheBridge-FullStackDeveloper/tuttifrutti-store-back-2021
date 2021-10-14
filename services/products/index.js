51957a24bdeae236051a0b3143fe88c82cfbe014
Giorgio Grassini <giorgiograssini@gmail.com>
2021-10-14 19:06:09 +0200

    feat: refactor in orders

--- services/products/index.js
+++ services/products/index.js
@@ -2,14 +2,9 @@ const router = require("express").Router();
 
 module.exports = (db) => {
   router.get("/keyword", require("./get-by-keyword")(db));
-
   router.post("/", require("./get-by-search")(db));
-
   router.get("/featured", require("./get-featured")(db));
   router.get("/", require("./getProducts")(db));
-  router.get('/orders', require('./getOrder')(db));
-  router.post("/:order", require("./addToOrder")(db));
-  router.get("/orders", require("./getOrdersInCart")(db));
   return router;
 };
 
