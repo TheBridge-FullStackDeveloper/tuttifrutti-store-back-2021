94d59ba9260717bd33de6d397a5f40d03dd0a562
maidersonn <maidersonn@gmail.com>
2021-10-14 20:03:16 +0200

    feat:updateToken query

--- query/auth.js
+++ query/auth.js
@@ -1,34 +1,34 @@
-const { sql } = require('slonik')
+const { sql } = require('slonik');
 
 const userExist = async (db, { email, username }) => {
   return await db.maybeOne(sql`
     SELECT * FROM users
     WHERE email = ${email} OR username = ${username}
-  `)
-}
+  `);
+};
 
 const createUser = async (db, { email, username, hash, token }) => {
   try {
-    const result = await userExist(db, { email, username })
-    if(result) throw new Error('Username or email already on use')
+    const result = await userExist(db, { email, username });
+    if (result) throw new Error('Username or email already on use');
     return await db.query(sql`
       INSERT INTO users ( email, username, hash, activation_token )
       VALUES ( ${email}, ${username}, ${hash}, ${token} )
-    `)    
+    `);
   } catch (e) {
-    console.info('> Error at "createUser" query:', e.message)
-    return false
+    console.info('> Error at "createUser" query:', e.message);
+    return false;
   }
-}
+};
 
 const confirmUser = async (db, { token }) => {
   try {
-    return await db.transaction( async tx => {
-      const {rowCount, rows} = await tx.query(sql`
+    return await db.transaction(async (tx) => {
+      const { rowCount, rows } = await tx.query(sql`
         SELECT * FROM users
         WHERE activation_token = ${token}
-      `)
-      if(!rowCount) throw new Error('invalid token')
+      `);
+      if (!rowCount) throw new Error('invalid token');
       await tx.query(sql`
         UPDATE users
         SET
@@ -37,19 +37,19 @@ const confirmUser = async (db, { token }) => {
           updated_at = now()
         WHERE
           activation_token = ${token}
-      `)
-      return rows
-    })
+      `);
+      return rows;
+    });
   } catch (e) {
-    console.info('> Error at "confirmUser" query:', e.message)
-    return false
+    console.info('> Error at "confirmUser" query:', e.message);
+    return false;
   }
-}
+};
 
 const getUserByEmailOrUsername = async (
   db,
-  mail = "",
-  username = "",
+  mail = '',
+  username = '',
   comparationFn
 ) => {
   try {
@@ -58,18 +58,34 @@ const getUserByEmailOrUsername = async (
     );
 
     if (!result) {
-      throw new Error("invalid credentials");
+      throw new Error('invalid credentials');
     }
 
     const isValidPassword = await comparationFn(result.hash);
 
     if (!isValidPassword) {
-      throw new Error("invalid credentials");
+      throw new Error('invalid credentials');
     }
 
     return result;
   } catch (error) {
-    console.info("error at getUserByEmail query:", error.message);
+    console.info('error at getUserByEmail query:', error.message);
+    return false;
+  }
+};
+
+const updateToken = async (
+  db,
+  token,
+  { email = 'default', username = 'bydefault' }
+) => {
+  try {
+    await db.query(
+      sql`UPDATE users SET activation_token = ${token} WHERE email LIKE ${email} OR username LIKE ${username}`
+    );
+    return true;
+  } catch (error) {
+    console.info('error at updateToken query:', error.message);
     return false;
   }
 };
@@ -78,4 +94,5 @@ module.exports = {
   createUser,
   confirmUser,
   getUserByEmailOrUsername,
-}
+  updateToken,
+};
