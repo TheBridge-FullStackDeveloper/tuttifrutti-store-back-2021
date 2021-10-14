ce6a91bf58efe035fdcd21e5d08801339b3eaa22
maidersonn <maidersonn@gmail.com>
2021-10-14 19:40:38 +0200

    feat: get for username or email

--- query/user.js
+++ query/user.js
@@ -1,19 +1,25 @@
-const { sql } = require("slonik");
+const { sql } = require('slonik');
 
-const getUserData = async (db, {username}) => {
-	try {
-		const { rows: user } = await db.maybeOne(sql`
+const getUserData = async (db, { username, email }) => {
+  let whereClause = '';
+  if (username) {
+    whereClause = sql`WHERE username = ${username}`;
+  } else {
+    whereClause = sql`WHERE email = ${email}`;
+  }
+
+  try {
+    const { rows: user } = await db.maybeOne(sql`
 			SELECT name, surname, username, email, address, postal_code, profile_pic, access_token
-			FROM users
-			WHERE username = ${username}
+			FROM users ${whereClause}
 		`);
-		return user
-	} catch (error) {
-		console.info('Error at getUserData:', error.message)
-		return false
-	}
-}
+    return user;
+  } catch (error) {
+    console.info('Error at getUserData:', error.message);
+    return false;
+  }
+};
 
 module.exports = {
-	getUserData,
-}
+  getUserData,
+};
