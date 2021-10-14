2a02bc31fe5e81d3614a9a8c02cf9727486cf347
maidersonn <maidersonn@gmail.com>
2021-10-14 19:38:42 +0200

    routing password forgotten

--- services/auth/index.js
+++ services/auth/index.js
@@ -1,11 +1,11 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
 module.exports = (db) => {
-
-  router.post("/register", require("./register")(db));
-  router.get("/confirmation/:token", require("./confirm")(db));
-  router.post("/login", require("./login")(db));
+  router.post('/register', require('./register')(db));
+  router.get('/confirmation/:token', require('./confirm')(db));
+  router.post('/login', require('./login')(db));
+  router.post('/password_forgotten', require('./password_forgotten')(db));
+  router.post('/password/request', require('./password_request')(db));
 
   return router;
 };
-
