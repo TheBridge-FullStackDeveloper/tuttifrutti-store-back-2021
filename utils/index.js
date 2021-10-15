354e7e375a5df1c4211cf5f303e81bb1bde2906a
maidersonn <maidersonn@gmail.com>
2021-10-15 15:35:37 +0200

    despistes

--- utils/index.js
+++ utils/index.js
@@ -1,16 +1,17 @@
-const upperCaseFn = str => str.replace(/(^\w|\s\w)/g, m => m.toUpperCase())
+const upperCaseFn = (str) => str.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase());
 
-const catcher = (fn) => async ({...args}) => {
-    try{
-      await fn({...args})
-    } catch (e) {
-      console.info('Error at query: ', error.message)
-      return false
+const catcher =
+  (fn) =>
+  async ({ ...args }) => {
+    try {
+      await fn({ ...args });
+    } catch (error) {
+      console.info('Error at query: ', error.message);
+      return false;
     }
-  }
-  
+  };
 
 module.exports = {
-    upperCaseFn,
-    catcher,
-}
+  upperCaseFn,
+  catcher,
+};
