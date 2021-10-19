9fab1b28962c2914e532bfc4d12bbf2b4dbb6f57
Juan Anton <jantonperez1980@gmail.com>
2021-10-19 21:16:11 +0200

    chore: merge conflict resolved

--- services/index.js
+++ services/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 
 module.exports = (db) => {
-<<<<<<< HEAD
   
   router.use("/products", require("./products")(db));
   router.use("/auth", require("./auth")(db));
@@ -9,13 +8,6 @@ module.exports = (db) => {
   router.use("/users", require("./users")(db));
   router.use("/orders", require("./orders")(db));
   
-=======
-  router.use('/products', require('./products')(db));
-  router.use('/auth', require('./auth')(db));
-  router.use('/cards', require('./cards')(db));
-  router.use('/users', require('./users')(db));
-  router.use('/orders', require('./products')(db));
->>>>>>> develop
 
   return router;
 };
