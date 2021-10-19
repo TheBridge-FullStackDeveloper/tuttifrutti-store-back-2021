1c00cb72f299ec3d9f1a46686112de8e46a0c1c9
maidersonn <maidersonn@gmail.com>
2021-10-19 14:50:58 +0200

    chore: fields names and endpoint

--- services/auth/index.js
+++ services/auth/index.js
@@ -4,8 +4,8 @@ module.exports = (db) => {
   router.post('/register', require('./register')(db));
   router.get('/confirmation/:token', require('./confirm')(db));
   router.post('/login', require('./login')(db));
-  router.post('/password_forgotten', require('./password_forgotten')(db));
-  router.post('/password/request', require('./password_request')(db));
+  router.post('/password/forgotten', require('./password-forgotten')(db));
+  router.post('/password/request', require('./password-request')(db));
 
   return router;
 };
