2b5715949b995ab352b96d21772fd8b83fb585ba
clgsg <clg_listas@yahoo.es>
2021-10-20 19:55:38 +0200

    chore: added urls to product_pic

--- scripts/seeds.js
+++ scripts/seeds.js
@@ -5,15 +5,14 @@ const { sql } = require("slonik");
 const seed = async () => {
   try {
     await db.query(sql`
-    INSERT INTO products (name, category, price, keywords)
-    VALUES
-    ('Chicle Boomer Fresa', 'chicle', '0.10', ARRAY ['fresa', 'boomer', 'chicle'] ),
-    ('Chicle Boomer Fresa/Nata', 'chicle', '0.10', ARRAY ['fresa', 'boomer', 'chicle', 'nata', 'dos'] ),
-    ('Chicle Boomer Menta', 'chicle', '0.10', ARRAY ['menta', 'boomer', 'chicle'] ),
-    ('Pipas Facundo 50g', 'pipas', '0.50', ARRAY ['pipas', 'sal'] ),
-    ('Pipas Facundo Aguasal 50g', 'pipas', '0.50', ARRAY ['pipas', 'aguasal'] ),
-    ('Piponazo 50g', 'pipas', '0.50', ARRAY ['pipas', 'sal'] ),
-    ('Piponazo Aguasal 50g', 'pipas', '0.50', ARRAY ['pipas', 'aguasal'] );
+    INSERT INTO products (name, category, price, product_pic, keywords)
+VALUES ('Chicle Boomer Fresa', 'chicle', '0.10', "https://www.google.com/url?sa=i&url=http%3A%2F%2Fproductonacionalespana.blogspot.com%2F2020%2F05%2Fpor-que-desaparecieron-los-chicles.html&psig=AOvVaw1LNlJVfT1xZejSmIoau-tZ&ust=1634836771154000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJDvw9y_2fMCFQAAAAAdAAAAABAD", ARRAY ['fresa', 'boomer', 'chicle'] ),
+       ('Chicle Boomer Fresa/Nata', 'chicle', '0.10', "https://www.google.com/imgres?imgurl=https%3A%2F%2Fturronesartesanos.es%2Fstorage%2Fproducts%2Fturron_chicle_nata.jpg&imgrefurl=https%3A%2F%2Fturronesartesanos.es%2Fturron-chicle-nata-fresa&tbnid=qvYd5yt3vOshjM&vet=12ahUKEwi7rOC_wNnzAhUKMRoKHUW2CjEQMygBegQIARAU..i&docid=3IjBKQIeb-hxVM&w=640&h=480&q=%22chicle%20fresa%20nata%22&hl=en&ved=2ahUKEwi7rOC_wNnzAhUKMRoKHUW2CjEQMygBegQIARAU", ARRAY ['fresa', 'boomer', 'chicle', 'nata', 'dos'] ),
+       ('Chicle Boomer Menta', 'chicle', '0.10', "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcloud10.todocoleccion.online%2Fcoleccionismo%2Ftc%2F2018%2F07%2F02%2F12%2F126855859_96818264.jpg%3Fsize%3D720x720%26crop%3Dtrue&imgrefurl=https%3A%2F%2Fwww.todocoleccion.net%2Fcoleccionismo%2Fchicle-chicles-boomer-general-confiteria-1990s-1992-1993-menta-i~x133068022&tbnid=yTHdlLrbnTwyQM&vet=12ahUKEwi_24TTwNnzAhVMeRoKHeWFCdMQMygMegQIARBt..i&docid=T9S1_vrEhJCFfM&w=720&h=720&q=%22chicle%20boomer%20menta%22&hl=en&ved=2ahUKEwi_24TTwNnzAhVMeRoKHeWFCdMQMygMegQIARBt", ARRAY ['menta', 'boomer', 'chicle'] ),
+       ('Pipas Facundo 50g', 'pipas', '0.50', "https://www.google.com/imgres?imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FI%2F81xeaRUJ5WL._AC_SY606_.jpg&imgrefurl=https%3A%2F%2Fwww.amazon.es%2FPipa-Girasol-Facundo-Bolsa-100G%2Fdp%2FB01IV0XFUC&tbnid=VQBoipKi6fNH2M&vet=12ahUKEwiGt_yCwdnzAhXR0YUKHUKoAyMQMygCegUIARClAQ..i&docid=-EBLvsnAKfdSyM&w=250&h=606&q=pipas%20facundo%20sal&hl=en&ved=2ahUKEwiGt_yCwdnzAhXR0YUKHUKoAyMQMygCegUIARClAQ", ARRAY ['pipas', 'sal'] ),
+       ('Pipas Facundo Aguasal 50g', 'pipas', '0.50', "https://www.google.com/imgres?imgurl=https%3A%2F%2Fartedulce.es%2Fwp-content%2Fuploads%2F2020%2F05%2F1126-Pipas-Aguasal-Bols.png&imgrefurl=https%3A%2F%2Fartedulce.es%2Fproducts%2Fpipa-bolsa-formato-familiar%2F&tbnid=K9fygKwQ-HyMOM&vet=12ahUKEwiw2d2QwdnzAhWC0YUKHUH8CEIQMygWegUIARDOAQ..i&docid=9-oLpiISBr_Z4M&w=150&h=250&itg=1&q=pipas%20facundo%20aguasal&hl=en&ved=2ahUKEwiw2d2QwdnzAhWC0YUKHUH8CEIQMygWegUIARDOAQ", ARRAY ['pipas', 'aguasal']),
+       ('Piponazo 50g', 'pipas', '0.50', "https://www.google.com/imgres?imgurl=https%3A%2F%2Fespanaencasa.com.es%2F1839%2Fgrefusa-piponazo-sal-150-grs.jpg&imgrefurl=https%3A%2F%2Fespanaencasa.com.es%2Fes%2Fhombre%2F468-grefusa-piponazo-sal-150-grs.html&tbnid=Bx2nKq_c4hbs_M&vet=12ahUKEwioxYGtwdnzAhWs8IUKHTOSDioQMygCegUIARCUAQ..i&docid=rVLlsjBlaWgQ7M&w=1082&h=1496&q=piponazo%20sal&hl=en&ved=2ahUKEwioxYGtwdnzAhWs8IUKHTOSDioQMygCegUIARCUAQ", ARRAY ['pipas', 'sal'] ),
+       ('Piponazo Aguasal 50g', 'pipas', '0.50', "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmacenio.com%2Fwp-content%2Fuploads%2F2020%2F09%2Fpiponazo-aguasal.png&imgrefurl=https%3A%2F%2Fmacenio.com%2Fproducto%2Fpiponazo-aguasal-100-g%2F&tbnid=hue-XzfvXuNQtM&vet=12ahUKEwipr4q4wdnzAhWT0YUKHVCXDTIQMygCegUIARCYAQ..i&docid=EP8s46-abX3_-M&w=1082&h=1496&q=piponazo%20aguasal&hl=en&ved=2ahUKEwipr4q4wdnzAhWT0YUKHVCXDTIQMygCegUIARCYAQ", ARRAY ['pipas', 'aguasal'] );
     INSERT INTO users (name, surname, email, username, hash, address, postal_code)
     VALUES
     ('Jorge', 'Nova', 'jnova@gmail.com', 'jnova', 'hash para prueba','C/Nueva 22, 3ºD, Madrid', '28001'),
