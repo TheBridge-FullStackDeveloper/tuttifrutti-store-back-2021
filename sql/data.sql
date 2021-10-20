f705ea97c43f0f65f4a967bf46237eaa2b5e7007
Cristina López <82091896+clgsg@users.noreply.github.com>
2021-10-20 20:33:04 +0200

    Chore/add product pics (#27)
    * chore: added urls to product_pic
    
    * chore: update urls for product pics
    
    Co-authored-by: clgsg <clg_listas@yahoo.es>

--- sql/data.sql
+++ sql/data.sql
@@ -1,17 +1,18 @@
-INSERT INTO products (name, category, price, keywords)
-VALUES ('Chicle Boomer Fresa', 'chicle', '0.10', ARRAY ['fresa', 'boomer', 'chicle'] ),
-       ('Chicle Boomer Fresa/Nata', 'chicle', '0.10', ARRAY ['fresa', 'boomer', 'chicle', 'nata', 'dos'] ),
-       ('Chicle Boomer Menta', 'chicle', '0.10', ARRAY ['menta', 'boomer', 'chicle'] ),
-       ('Pipas Facundo 50g', 'pipas', '0.50', ARRAY ['pipas', 'sal'] ),
-       ('Pipas Facundo Aguasal 50g', 'pipas', '0.50', ARRAY ['pipas', 'aguasal'] ),
-       ('Piponazo 50g', 'pipas', '0.50', ARRAY ['pipas', 'sal'] ),
-       ('Piponazo Aguasal 50g', 'pipas', '0.50', ARRAY ['pipas', 'aguasal'] );
+INSERT INTO products (name, category, price, product_pic, keywords)
+VALUES
+        ('Chicle Boomer Fresa', 'chicle', '0.10', 'https://1.bp.blogspot.com/-MmB7MtVVKrU/XseeoES2TlI/AAAAAAAAAKE/8tCsr-i4iw85J_KCNDHg5LiqBON528C0gCLcBGAsYHQ/s1600/45449129_21939348%2B%25281%2529.jpg', ARRAY ['fresa', 'boomer', 'chicle'] ),
+       ('Chicle Boomer Fresa/Nata', 'chicle', '0.10', 'https://turronesartesanos.es/storage/products/turron_chicle_nata.jpg', ARRAY ['fresa', 'boomer', 'chicle', 'nata', 'dos'] ),
+       ('Chicle Boomer Menta', 'chicle', '0.10', 'https://cloud10.todocoleccion.online/coleccionismo/tc/2018/07/02/12/126855859_96818264.jpg?size=720x720&crop=true', ARRAY ['menta', 'boomer', 'chicle'] ),
+       ('Pipas Facundo 50g', 'pipas', '0.50', 'https://m.media-amazon.com/images/I/81xeaRUJ5WL._AC_SY606_.jpg', ARRAY ['pipas', 'sal'] ),
+       ('Pipas Facundo Aguasal 50g', 'pipas', '0.50', 'https://images.ssstatic.com/pipas-extra-grandes-facundo-85g-43770280z0-17214667.jpg', ARRAY ['pipas', 'aguasal']),
+       ('Piponazo 50g', 'pipas', '0.50', 'https://espanaencasa.com.es/1839/grefusa-piponazo-sal-150-grs.jpg', ARRAY ['pipas', 'sal'] ),
+       ('Piponazo Aguasal 50g', 'pipas', '0.50', 'https://macenio.com/wp-content/uploads/2020/09/piponazo-aguasal.png', ARRAY ['pipas', 'aguasal'] );
 
 INSERT INTO users (name, surname, email, username, hash, address, postal_code)
 VALUES ('Jorge', 'Nova', 'jnova@gmail.com', 'jnova', 'hash para prueba','C/Nueva 22, 3ºD, Madrid', '28001'),
        ('Cristina', 'Nosé', 'cris@gmail.com', 'cris', 'hash para prueba', 'C/Vieja 22, 3º Madrid', '28002'),
         ('Diego', 'González', 'diego@gmail.com', 'dgz', 'hash para prueba', 'C/Intermedia 22, 3º Madrid', '28002');
-        
+
 INSERT INTO orders (state)
 VALUES ('pending'), ('sent'), ('delivered'), ('canceled');
 
