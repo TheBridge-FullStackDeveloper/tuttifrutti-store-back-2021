efe881430edd5cdfd8ec5c4284e5fd7a146fe3b3
maidersonn <maidersonn@gmail.com>
2021-10-25 21:22:40 +0200

    fix: ssl=1

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const slonik = require('slonik');
 
 const slonikURL =
   process.env.NODE_ENV === 'production'
-    ? process.env.DATABASE_URL
+    ? process.env.PRODUCTION_URL
     : process.env.DB_URL;
 
 const db = slonik.createPool(slonikURL);
